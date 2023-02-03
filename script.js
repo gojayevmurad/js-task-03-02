0afc516d73348e41e3cba1ec40062a77e703fd9e
Murad Qocayev <muradqocayevi20@gmail.com>
2023-02-03 13:46:30 +0300

    third commit

--- script.js
+++ script.js
@@ -49,3 +49,57 @@
 //   arr2[i] == arr1[i] ? true : diffArr.push(arr2[i]);
 // }
 // console.log(diffArr);
+
+// n = 7
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 1 2 3 4 5 6 7
+
+// var a = +prompt("Enter number:");
+// for (var i = 1; i <= a; i++) {
+//   let b = ''
+//   for (let k = 1; k <= i; k++) {
+//      b+=k
+//   }
+//   console.log(b)
+// }
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+// * * * *
+// *     *
+// *     *
+// *     *
+// * * * *
+
+// let numX = +prompt('Enter number: "Y" ');
+// let numY = +prompt('Enter number: "X" ');
+// let a = "";
+// let confirmEl = confirm("Empty");
+
+// if (confirmEl) {
+//   for (let x = 0; x < numX; x++) {
+//     for (let y = 0; y < numY; y++) {
+//       if (y == 0 || y == numY - 1 || x == 0 || x == numX - 1) {
+//         a += "x ";
+//       } else {
+//         a += "  ";
+//       }
+//     }
+//     a += "\n";
+//   }
+// } else {
+//   for (let i = 0; i < numX; i++) {
+//     a += "* ".repeat(numY) + "\n";
+//   }
+// }
+// console.log(a);
